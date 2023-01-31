08436ca641d578e27fcff4bcd9ac4b1094617749
Dan Coates <dcoates72@gmail.com>
2023-01-31 15:09:37 -0800

    Make usable for Windows paths

--- torsion_params.py
+++ torsion_params.py
@@ -13,20 +13,27 @@ filename_stabilized_video=os.path.join( output_directory, "COST_rotvideo.avi" )
 filename_stabilized_avg=os.path.join( output_directory, "COST_rotavg.tif" )
 
 
-# Settings inside WSL (Windows subsystem for Linux)
+## Settings inside WSL (Windows subsystem for Linux) ##
+
 # volume_directory="/mnt/f/Torsion_correction_data/Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms/registervol_combine"
 # volumes=os.path.join( volume_directory,'*','*registeredvol.mat')
 #volumes_rotated_directory="/mnt/f/Torsion_correction_data/Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms/torsion_fixed_cropped"
 
-volume_directory="/mnt/f/Torsion_correction_data/Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms/registervol_combine_padded"
+#volume_directory=r"/mnt/f/Torsion_correction_data/Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms/registervol_combine_padded"
+#volumes=os.path.join( volume_directory,'*registeredvol.mat')
+#volumes_rotated_directory="/mnt/f/Torsion_correction_data/Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms/torsion_fixed_padded"
+
+## Settings inside Windows shell
+
+volume_directory=r"F:\Torsion_correction_data\Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms\registervol_combine_padded"
 volumes=os.path.join( volume_directory,'*registeredvol.mat')
-volumes_rotated_directory="/mnt/f/Torsion_correction_data/Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms/torsion_fixed_padded"
+volumes_rotated_directory=r"F:\Torsion_correction_data\Image_00001_AO001R_896_512_600_50_1.57_12000_-ve0.13_Nd0.0_20ms\torsion_fixed_padded"
 
 padding_pixels_each_side=50
 volume_rot90=1
 
 # Optional:
-volumes_to_process=None #range(1,11,1)  # If you specify None here, all volumes will be processed
+volumes_to_process=range(1,500,1)  # Can specify a subset. Okay if larger than data.5
 layers_to_process=range(13,18) # Z-planes. others are left as zero
 filename_transforms=os.path.join( output_directory, "xform_info.pickle" )
 do_rot90=0 # Rotate the COST video, since it's rot90 from the raw volumes. Don't use yet.
