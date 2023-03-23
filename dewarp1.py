9e1c4523d6be39d68c39e25ab80256a27ca9cb21
Dan Coates <dcoates72@gmail.com>
2023-03-23 17:18:07 -0600

    Working standalone single pair dewarp, output matrix to a mat file

--- dewarp1.py
+++ dewarp1.py
@@ -21,38 +21,24 @@ import torsion_apply
 mode="both" # compute|apply|both
 
 if __name__ == "__main__":
-    file_ref=sys.argv[-2]
-    file_target=sys.argv[-1]
-    print("in=",file_ref)
-    print("out=",file_target)
-      
-    shifted = np.zeros( 2, size(file_ref)[0], size(file_ref)[1]);
+    filename_ref=sys.argv[-3]
+    filename_target=sys.argv[-2]
+    filename_xf=sys.argv[-1]
     
-    ref=io.loadmat(file_ref)
-    target=io.loadmat(file_target)
+    ref=io.loadmat(filename_ref)['data']
+    target=io.loadmat(filename_target)['data']
+
+    shifted = np.zeros( (np.shape(ref)[0], np.shape(ref)[1], 2));
+    print (np.shape(shifted))
     
+    shifted[...,0]=ref
+    shifted[...,1]=target
+
     # Determine and remove residual torsion.
     mask_data = (shifted>0).astype("float32")
     shifted, xforms, inliers, itk_xforms = optimizer_stack_align(shifted, mask_data,
-                                                        reference_idx=reference_frame_idx)
+                                                        reference_idx=0)
    
-    save_video(torsion_params.filename_stabilized_video,(shifted*255).astype("uint8"),30 )
-
-    # Clamp our data.
-    mask_data[mask_data < 0] = 0
-    mask_data[mask_data >= 1] = 1
+    io.savemat(filename_xf, {'xforms':xforms})
     
-    io.savemat('xf1.mat', {'xforms':xforms})
-    
-        overlap_map, sum_map = weighted_z_projection(mask_data, mask_data)
-        avg_im, sum_map = weighted_z_projection(shifted, mask_data)
-    
-        cv2.imwrite(torsion_params.filename_stabilized_avg, (avg_im*255).astype("uint8"))
-        #im_conf = Image.fromarray((avg_im * 255).astype("uint8"), "L")
-        #im_conf.putalpha(Image.fromarray((overlap_map * 255).astype("uint8"), "L"))
-        #im_conf.save("M:\\Dropbox (Personal)\\Research\\Torsion_Distortion_Correction\\COST_rawvideo_reavg.png")
-        #im_conf.save("data/COST_rawvideo_reavg.png")
-
-    if do_apply:
-        torsion_apply.do_apply(torsion_params)
         
